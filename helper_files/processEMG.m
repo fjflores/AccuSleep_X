56070e8b15f5d0545a1c9c179403f54f53b5b768
Francisco J. Flores <fjflores@mit.edu>
2023-07-24 21:08:59 -0400

    eliminates EMG filtering. Use 200-700 signmal.

--- helper_files/processEMG.m
+++ helper_files/processEMG.m
@@ -1,4 +1,4 @@
-function [processedEMG] = processEMG(EMG, SR, epochLen)
+function processedEMG = processEMG( EMG, SR, epochLen )
 % processEMG  Compute the RMS in each epoch of the EMG signal
 % Zeke Barger 100119
 %
@@ -11,23 +11,29 @@ function [processedEMG] = processEMG(EMG, SR, epochLen)
 %   processedEMG - the processed EMG signal
 
 % filter the EMG between 20 and 50 Hz
-d = designfilt('bandpassfir','DesignMethod','kaiserwin','PassbandFrequency1',...
-    20, 'PassbandFrequency2', 50, 'PassbandRipple',0.1, 'SampleRate', SR, ...
-    'StopbandAttenuation1',60,'StopbandAttenuation2',60,...
-    'StopbandFrequency1',16.86, 'StopbandFrequency2',52.198);
-EMG = filtfilt(d, EMG);
+% d = designfilt('bandpassfir','DesignMethod','kaiserwin','PassbandFrequency1',...
+%     20, 'PassbandFrequency2', 50, 'PassbandRipple',0.1, 'SampleRate', SR, ...
+%     'StopbandAttenuation1',60,'StopbandAttenuation2',60,...
+%     'StopbandFrequency1',16.86, 'StopbandFrequency2',52.198);
+% EMG = filtfilt(d, EMG);
 
 % truncate EMG to a multiple of SR
-samplesPerEpoch = SR*epochLen;
-EMG = EMG(1:(length(EMG)-mod(length(EMG), samplesPerEpoch)));
+samplesPerEpoch = SR * epochLen;
+EMG = EMG( 1 : ( length( EMG ) - mod( length( EMG ), samplesPerEpoch ) ) );
 
 % calculate log(rms) in each time window
 % we can do this faster if the number of samples per epoch is an integer
-if samplesPerEpoch == floor(samplesPerEpoch)
-    processedEMG = log(rms(reshape(EMG,samplesPerEpoch,length(EMG)/samplesPerEpoch)));
+if samplesPerEpoch == floor( samplesPerEpoch )
+    processedEMG = log( rms( reshape(...
+        EMG, samplesPerEpoch, length( EMG ) / samplesPerEpoch ) ) );
+
 else
     processedEMG = zeros(1,floor(length(EMG)/samplesPerEpoch));
-    for i = 1:length(processedEMG)
-        processedEMG(i)=log(rms(EMG(floor((i-1)*samplesPerEpoch + 1):floor(i*samplesPerEpoch))));
+
+    for i = 1:length( processedEMG )
+        processedEMG( i ) = log( rms(...
+            EMG( floor( ( i - 1 ) * samplesPerEpoch + 1 )...
+            : floor( i * samplesPerEpoch ) ) ) );
+
     end
 end
